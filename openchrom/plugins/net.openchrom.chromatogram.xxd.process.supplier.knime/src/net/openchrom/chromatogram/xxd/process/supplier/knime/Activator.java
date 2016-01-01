45d710cabb6768c2d9aaf9f7d0fc098fc2a651c2
Dr. Philip Wenig <philip.wenig@openchrom.net>
2016-01-01 14:10:38 +0100

    Happy new year 2016!

--- openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.knime/src/net/openchrom/chromatogram/xxd/process/supplier/knime/Activator.java
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.knime/src/net/openchrom/chromatogram/xxd/process/supplier/knime/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Lablicate UG (haftungsbeschränkt).
+ * Copyright (c) 2015, 2016 Lablicate UG (haftungsbeschränkt).
  * 
  * This library is free
  * software; you can redistribute it and/or modify it under the terms of the GNU
