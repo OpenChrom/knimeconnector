aa1e30bbc599c2e7fc9a715020ed3363850582f3
Dr. Philip Wenig <philip.wenig@openchrom.net>
2017-02-17 18:29:50 +0100

    The MSD I/O nodes have been added.

--- openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.knime.ui/src/net/openchrom/chromatogram/xxd/process/supplier/knime/ui/Activator.java
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.knime.ui/src/net/openchrom/chromatogram/xxd/process/supplier/knime/ui/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2017 Lablicate GmbH.
+ * Copyright (c) 2016, 2017 Lablicate GmbH.
  * 
  * This library is free
  * software; you can redistribute it and/or modify it under the terms of the GNU
@@ -11,6 +11,9 @@
  * details. You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contributors:
+ * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package net.openchrom.chromatogram.xxd.process.supplier.knime.ui;
 
--- openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.knime/src/net/openchrom/chromatogram/xxd/process/supplier/knime/Activator.java
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.knime/src/net/openchrom/chromatogram/xxd/process/supplier/knime/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, 2017 Lablicate GmbH.
+ * Copyright (c) 2016, 2017 Lablicate GmbH.
  * 
  * This library is free
  * software; you can redistribute it and/or modify it under the terms of the GNU
