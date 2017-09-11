be1062731bdff0e487a55a8d6ad9e0cf5f757077
Jan Holy <holyjan3@fel.cvut.cz>
2017-09-11 15:17:35 +0200

    improve file dialog,
    Dialog display all file by default  which are supported.

--- openchrom/plugins/net.openchrom.xxd.process.supplier.knime.ui/src/net/openchrom/xxd/process/supplier/knime/ui/io/msd/ChromatogramReaderMSDNodeDialog.java
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.knime.ui/src/net/openchrom/xxd/process/supplier/knime/ui/io/msd/ChromatogramReaderMSDNodeDialog.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2017 Lablicate GmbH.
- * 
+ *
  * This library is free
  * software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation;
@@ -11,13 +11,14 @@
  * details. You should have received a copy of the GNU General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * 
+ *
  * Contributors:
  * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package net.openchrom.xxd.process.supplier.knime.ui.io.msd;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -42,10 +43,18 @@ public class ChromatogramReaderMSDNodeDialog extends DefaultNodeSettingsPane {
 		//
 		String[] validExtensions;
 		if(extensions.size() > 0) {
-			validExtensions = extensions.toArray(new String[extensions.size()]);
-			Arrays.sort(validExtensions);
+			validExtensions = extensions.toArray(new String[extensions.size() + 1]);
+			Arrays.sort(validExtensions, Comparator.nullsFirst(Comparator.naturalOrder()));
+			StringBuilder sb = new StringBuilder();
+			int i = 1;
+			for(i = 1; i < validExtensions.length - 1; i++) {
+				sb.append(validExtensions[i]);
+				sb.append('|');
+			}
+			sb.append(validExtensions[i]);
+			validExtensions[0] = sb.toString();
 		} else {
-			validExtensions = new String[]{".*"};
+			validExtensions = new String[]{};
 		}
 		//
 		DialogComponentFileChooser dialogComponentFileChooser = new DialogComponentFileChooser(ChromatogramReaderMSDNodeModel.SETTING_CHROMATOGRAM_FILE_INPUT, "", validExtensions);
