243a7432ca0f8eba8ed342a32c2d6fd379724221
Jan Holy <jan.holy91@gmail.com>
2018-03-01 17:25:55 +0100

    add scan xir port object

---
+++ openchrom/plugins/net.openchrom.xxd.process.supplier.knime/src/net/openchrom/xxd/process/supplier/knime/model/ScanXIRPortObjectSpec.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Jan Holy - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.xxd.process.supplier.knime.model;
+
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.ModelContentRO;
+import org.knime.core.node.ModelContentWO;
+import org.knime.core.node.port.AbstractSimplePortObjectSpec;
+
+public class ScanXIRPortObjectSpec extends AbstractSimplePortObjectSpec {
+
+	public ScanXIRPortObjectSpec() {
+	}
+
+	@Override
+	protected void save(ModelContentWO model) {
+
+	}
+
+	@Override
+	protected void load(ModelContentRO model) throws InvalidSettingsException {
+
+	}
+}
