14f151a68ba465d9b15d68546dee832398a0aa6f
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-09-23 16:48:32 +0200

    Expose Processtype support from activator

--- openchrom/plugins/net.openchrom.knime.base/src/net/openchrom/knime/base/Activator.java
+++ openchrom/plugins/net.openchrom.knime.base/src/net/openchrom/knime/base/Activator.java
@@ -1,7 +1,12 @@
 package net.openchrom.knime.base;
 
+import org.eclipse.chemclipse.processing.ProcessorFactory;
+import org.eclipse.chemclipse.xxd.process.support.ProcessTypeSupport;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -14,6 +19,8 @@ public class Activator implements BundleActivator {
 	// The shared instance
 	private static Activator plugin;
 	
+	private static ServiceTracker<ProcessorFactory, ProcessTypeSupport> processTypeSupportTracker;
+	
 	/**
 	 * The constructor
 	 */
@@ -23,12 +30,38 @@ public class Activator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
 		plugin = this;
+		processTypeSupportTracker = new ServiceTracker<>(context, ProcessorFactory.class, new ServiceTrackerCustomizer<ProcessorFactory, ProcessTypeSupport>() {
+
+			@Override
+			public ProcessTypeSupport addingService(ServiceReference<ProcessorFactory> reference) {
+				ProcessorFactory service = context.getService(reference);
+				if (service != null) {
+					return new ProcessTypeSupport(service);
+				}
+				return null;
+			}
+
+			@Override
+			public void modifiedService(ServiceReference<ProcessorFactory> reference, ProcessTypeSupport service) {
+			}
+
+			@Override
+			public void removedService(ServiceReference<ProcessorFactory> reference, ProcessTypeSupport service) {
+				context.ungetService(reference);
+				
+			}
+		});
+		processTypeSupportTracker.open();
+	}
+	
+	public static ProcessTypeSupport getProcessTypeSupport() {
+		return processTypeSupportTracker.getService();
 	}
 
 	
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
-		
+		processTypeSupportTracker.close();
 	}
 
 	/**
