d80d22480a5b1674d5a52670705cbf6ab1339b5f
Alexander Kerner <alexkerner1000@gmail.com>
2019-09-24 16:07:10 +0200

    Remove ProcessTypeSupport for now
    ProcessTypeSupport has a huge dependency footprint, dismiss it for now.

--- openchrom/plugins/net.openchrom.knime.base/src/net/openchrom/knime/base/Activator.java
+++ openchrom/plugins/net.openchrom.knime.base/src/net/openchrom/knime/base/Activator.java
@@ -1,12 +1,7 @@
 package net.openchrom.knime.base;
 
-import org.eclipse.chemclipse.processing.ProcessorFactory;
-import org.eclipse.chemclipse.xxd.process.support.ProcessTypeSupport;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.osgi.util.tracker.ServiceTracker;
-import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -18,50 +13,23 @@ public class Activator implements BundleActivator {
 
 	// The shared instance
 	private static Activator plugin;
-	
-	private static ServiceTracker<ProcessorFactory, ProcessTypeSupport> processTypeSupportTracker;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
 
-
+	@Override
 	public void start(BundleContext context) throws Exception {
 		plugin = this;
-		processTypeSupportTracker = new ServiceTracker<>(context, ProcessorFactory.class, new ServiceTrackerCustomizer<ProcessorFactory, ProcessTypeSupport>() {
 
-			@Override
-			public ProcessTypeSupport addingService(ServiceReference<ProcessorFactory> reference) {
-				ProcessorFactory service = context.getService(reference);
-				if (service != null) {
-					return new ProcessTypeSupport(service);
-				}
-				return null;
-			}
-
-			@Override
-			public void modifiedService(ServiceReference<ProcessorFactory> reference, ProcessTypeSupport service) {
-			}
-
-			@Override
-			public void removedService(ServiceReference<ProcessorFactory> reference, ProcessTypeSupport service) {
-				context.ungetService(reference);
-				
-			}
-		});
-		processTypeSupportTracker.open();
-	}
-	
-	public static ProcessTypeSupport getProcessTypeSupport() {
-		return processTypeSupportTracker.getService();
 	}
 
-	
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
-		processTypeSupportTracker.close();
+
 	}
 
 	/**
