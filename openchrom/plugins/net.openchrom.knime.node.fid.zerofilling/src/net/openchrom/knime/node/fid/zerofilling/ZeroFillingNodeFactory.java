f932afa098cfaa24d8d63f89cb2e71054ff3ff9a
Alexander Kerner <alexkerner1000@gmail.com>
2019-10-17 17:53:56 +0200

    Introduce the Zero Filling node

---
+++ openchrom/plugins/net.openchrom.knime.node.fid.zerofilling/src/net/openchrom/knime/node/fid/zerofilling/ZeroFillingNodeFactory.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *Alexander Kerner - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.knime.node.fid.zerofilling;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * {@link NodeFactory} for {@link ZeroFillingNodeModel}.
+ * 
+ * @author Alexander Kerner
+ *
+ */
+public class ZeroFillingNodeFactory extends NodeFactory<ZeroFillingNodeModel> {
+
+	@Override
+	public ZeroFillingNodeModel createNodeModel() {
+		return new ZeroFillingNodeModel();
+	}
+
+	@Override
+	protected int getNrNodeViews() {
+		return 0;
+	}
+
+	@Override
+	public NodeView<ZeroFillingNodeModel> createNodeView(int viewIndex, ZeroFillingNodeModel nodeModel) {
+		return null;
+	}
+
+	@Override
+	protected boolean hasDialog() {
+		return false;
+	}
+
+	@Override
+	protected NodeDialogPane createNodeDialogPane() {
+		return null;
+	}
+
+}
---
+++ openchrom/plugins/net.openchrom.knime.node.fid.zerofilling/src/net/openchrom/knime/node/fid/zerofilling/ZeroFillingNodeModel.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Lablicate GmbH.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Alexander Kerner - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.knime.node.fid.zerofilling;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+
+import net.openchrom.knime.node.base.FIDPortObject;
+import net.openchrom.knime.node.base.GenericPortObjectSpec;
+import net.openchrom.knime.node.base.ProcessorAdapter;
+import net.openchrom.nmr.processing.supplier.base.core.ZeroFillingProcessor;
+
+/**
+ * {@link NodeModel} for the FID to Table node.
+ * 
+ * @author Alexander Kerner
+ *
+ */
+public class ZeroFillingNodeModel extends NodeModel {
+
+	private static final NodeLogger logger = NodeLogger.getLogger(ZeroFillingNodeModel.class);
+
+	public ZeroFillingNodeModel() {
+
+		super(new PortType[]{FIDPortObject.TYPE}, new PortType[]{FIDPortObject.TYPE});
+	}
+
+	@Override
+	protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+
+		final GenericPortObjectSpec portOne = new GenericPortObjectSpec();
+		return new PortObjectSpec[]{portOne};
+	}
+
+	@Override
+	protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
+
+		return ProcessorAdapter.adaptFIDinFIDout(new ZeroFillingProcessor(), inObjects, exec, logger);
+	}
+
+	@Override
+	protected void loadInternals(final File nodeInternDir, final ExecutionMonitor exec) throws IOException, CanceledExecutionException {
+
+		logger.debug(this.getClass().getSimpleName() + ": Load internals");
+	}
+
+	@Override
+	protected void saveInternals(final File nodeInternDir, final ExecutionMonitor exec) throws IOException, CanceledExecutionException {
+
+		logger.debug(this.getClass().getSimpleName() + ": Save internals");
+	}
+
+	@Override
+	protected void saveSettingsTo(final NodeSettingsWO settings) {
+
+		logger.debug(this.getClass().getSimpleName() + ": Saving settings");
+	}
+
+	@Override
+	protected void validateSettings(final NodeSettingsRO settings) throws InvalidSettingsException {
+
+		logger.debug(this.getClass().getSimpleName() + ": Validate settings");
+	}
+
+	@Override
+	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings) throws InvalidSettingsException {
+
+		logger.debug(this.getClass().getSimpleName() + ": Loading validated settings");
+	}
+
+	@Override
+	protected void reset() {
+
+		logger.debug(this.getClass().getSimpleName() + ": OnReset");
+	}
+}
